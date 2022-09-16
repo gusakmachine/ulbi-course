2cb91c8c34605f8eac894ddbc71f8a2512597d84
mlitvinuk <utorrentfilibusters@gmail.com>
2022-09-16 13:47:54 +0300

    Feature-Slice architecture installed

--- config/build/buildResolvers.ts
+++ config/build/buildResolvers.ts
@@ -1,7 +1,14 @@
 import webpack from "webpack";
+import {BuildOptions} from "./types/config";
 
-export default function buildResolvers(): webpack.ResolveOptions {
+export default function buildResolvers(options: BuildOptions): webpack.ResolveOptions {
     return {
         extensions: ['.js', '.ts', '.tsx',],
+        alias: {},
+        preferAbsolute: true,
+        modules: [
+            options.paths.src, 'node_modules',
+        ],
+        mainFiles: ['index'],
     };
 }
